235981e7f10f2b4d6db784ef55c472881cdab9f9
Cavlheiro <151409968+Cavlheiro@users.noreply.github.com>
2026-03-15 20:35:05 -0300

    Create aula-01-registradores.s

---
+++ fundamentos/aula-01-registradores.s
@@ -0,0 +1,6 @@
+.global _start
+_start:
+
+@Aula 01-registradores
+mov r0, #50 @ mov indica que o registrador r0 está recebendo(=) o valor #50 | python: r0 = 50
+mov r1, r0 @ indica que r1 está recebendo(=) o valor de r0 | python: r1 = r0
